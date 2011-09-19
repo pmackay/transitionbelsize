a90243502868618a79b98c3baa7e3492da8dfdaa
Paul Mackay <pauljmackay@gmail.com>
2011-09-19 22:22:58 +0100

    Improve OG mission display.

--- sites/all/themes/tb/templates/og-mission.tpl.php
+++ sites/all/themes/tb/templates/og-mission.tpl.php
@@ -1,10 +1,10 @@
 <?php if (!empty($mission)): ?>
   <div id="mission" class="og-mission">
   	<?php if($image): ?>
-  		<div><?php print $image; ?></div>
+  		<div style="float: right; padding: 5px"><?php print $image; ?></div>
   	<?php endif; ?>
-  	<div><span>Manager:<span> <?php print $manager; ?></div>
-  	<div><span>Members:<span> <?php print $members; ?></div>
+  	<div style="font-size: 1.3em;"><span>Manager:<span> <?php print $manager; ?></div>
+  	<div style="font-size: 1.3em;"><span>Members:<span> <?php print $members; ?></div>
   	<?php print $mission; ?>
   </div>
 <?php endif; ?>
